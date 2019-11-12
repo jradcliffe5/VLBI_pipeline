eff64a53bc66dc00ffc22b98c92df5fec345185d
Jack Radcliffe <jackradcliffe@jacks-mbp.up.ac.za>
2019-11-12 15:26:27 +0200

    added import evn data

--- EVN/batch_imp_fitsidi.sh
+++ EVN/batch_imp_fitsidi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -N 1-1
 #SBATCH --tasks-per-node 12
-#SBATCH -J import_fitsidi
+#SBATCH -J EVN_imp_fits
 #SBATCH -m cyclic
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=jack.radcliffe@manchester.ac.uk
@@ -11,7 +11,7 @@
 inbase=eg078g
 
 
-/usr/bin/singularity exec /data/exp_soft/containers/casa-stable-5.6.0-149.simg  "casa" --nologger --log2term --nogui -c append_tsys.py $inbase".antab" $inbase"_1_1.IDI*"
-/usr/bin/singularity exec /data/exp_soft/containers/casa-stable-5.6.0-149.simg  "casa" --nologger --log2term --nogui -c gc.py $inbase".antab" $inbase".gcal"
-/usr/bin/singularity exec /data/exp_soft/containers/casa-stable-5.6.0-149.simg  "casa" --nologger --log2term --nogui -c flag.py $inbase".uvflg" $inbase".flags.txt"
-/usr/bin/singularity exec /data/exp_soft/containers/casa-stable-5.6.0-149.simg  "casa" --nologger --log2term --nogui -c run_imp_fitsidi.py $inbase
+/usr/bin/singularity exec /data/exp_soft/containers/casa-stable-5.6.0-60.simg  "casa" --nologger --log2term --nogui -c append_tsys.py $inbase".antab" $inbase"_1_1.IDI*"
+/usr/bin/singularity exec /data/exp_soft/containers/casa-stable-5.6.0-60.simg  "casa" --nologger --log2term --nogui -c gc.py $inbase".antab" $inbase".gcal"
+/usr/bin/singularity exec /data/exp_soft/containers/casa-stable-5.6.0-60.simg  "casa" --nologger --log2term --nogui -c flag.py $inbase".uvflg" $inbase".flags.txt"
+/usr/bin/singularity exec /data/exp_soft/containers/casa-stable-5.6.0-60.simg  "casa" --nologger --log2term --nogui -c run_imp_fitsidi.py $inbase
