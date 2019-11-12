dcc1e5a5cb72fc317871cdd9aac85c87fc70108e
Jack Radcliffe <jackradcliffe@jacks-mbp.up.ac.za>
2019-11-12 15:24:27 +0200

    added import evn data

--- EVN/batch_imp_fitsidi.sh
+++ EVN/batch_imp_fitsidi.sh
@@ -8,4 +8,10 @@
 #SBATCH -o importfits.sh.stdout.log
 #SBATCH -e importfits.sh.stderr.log
 #Run the application:
-/usr/bin/singularity exec /data/exp_soft/containers/casa-stable-5.5.0-149.simg  "casa" --nologger --log2term --nogui -c imp_fitsidi.py
+inbase=eg078g
+
+
+/usr/bin/singularity exec /data/exp_soft/containers/casa-stable-5.6.0-149.simg  "casa" --nologger --log2term --nogui -c append_tsys.py $inbase".antab" $inbase"_1_1.IDI*"
+/usr/bin/singularity exec /data/exp_soft/containers/casa-stable-5.6.0-149.simg  "casa" --nologger --log2term --nogui -c gc.py $inbase".antab" $inbase".gcal"
+/usr/bin/singularity exec /data/exp_soft/containers/casa-stable-5.6.0-149.simg  "casa" --nologger --log2term --nogui -c flag.py $inbase".uvflg" $inbase".flags.txt"
+/usr/bin/singularity exec /data/exp_soft/containers/casa-stable-5.6.0-149.simg  "casa" --nologger --log2term --nogui -c run_imp_fitsidi.py $inbase
