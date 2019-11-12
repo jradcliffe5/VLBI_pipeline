6d19a039a3f499cb34aa3e600f22f25d2b2e8285
Jack Radcliffe <jackradcliffe@jacks-mbp.up.ac.za>
2019-11-12 16:17:59 +0200

    changes

--- EVN/run_imp_fitsidi.py
+++ EVN/run_imp_fitsidi.py
@@ -6,5 +6,6 @@ for i in os.listdir('./'):
 	if i.startswith('%s_1_1'%inbase):
 		idifiles.append('%s'%(i))
 
+os.system('rm -r %s.ms'%(inbase))
 importfitsidi(fitsidifile=idifiles,vis='%s.ms'%(inbase),
 	          constobsid=False, scanreindexgap_s=15.0)
