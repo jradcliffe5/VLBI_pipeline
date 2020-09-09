615e78dd1d4d8f2a1d99185b51dc78181e34c26e
Jack Radcliffe <jackradcliffe@Jacks-MacBook-Pro.local>
2020-09-09 14:17:42 +0200

    added changes

--- run_phase_referencing.py
+++ run_phase_referencing.py
@@ -85,7 +85,7 @@ for i in range(len(fields)):
 					xax = ['freq','time']
 				else:
 					xax = ['time']
-				for k in ['delay','phase','rate']:
+				for k in ['delay','phase','rate','disp']:
 					for l in xax:
 						plotcaltable(caltable=caltable,yaxis='%s'%k,xaxis='%s'%l,plotflag=True,msinfo=msinfo,figfile='%s-%s_vs_%s.pdf'%(caltable,k,l))
 
