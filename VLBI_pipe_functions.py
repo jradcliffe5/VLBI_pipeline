22902fe83409044fde4dad54512092a0cf1e588c
Jack Radcliffe <jackradcliffe@MacBook-Pro.local>
2021-06-23 18:04:25 +0200

    changes

--- VLBI_pipe_functions.py
+++ VLBI_pipe_functions.py
@@ -2145,7 +2145,7 @@ def apply_to_all(prefix,files,tar,params,casa6,parallel):
 	rmdirs(['%s/%s_presplit.ms'%(cwd,i),'%s/%s_presplit.ms.flagversions'%(cwd,i)])
 
 	if params['apply_to_all']['pbcor']['backup_caltables'] == True:
-		append_tar_file(buf=pbcor_table.split('/')[-1],"%s_caltables.tar.gz"%p_c,output_path='./',replace=True)
+		append_tar_file(buf=pbcor_table.split('/')[-1],filename="%s_caltables.tar.gz"%p_c,output_path='./',replace=True)
 	
 
 def append_tar_file(buf, file_name, output_path, replace=True):
