330d611aed7dc4d99f6450f931f48eb91e508a9d
jradcliffe5 <jack.radcliffe@manchester.ac.uk>
2024-11-12 11:49:43 +0300

    mtmfs support for wsclean

--- run_phase_referencing.py
+++ run_phase_referencing.py
@@ -285,7 +285,7 @@ for i in range(len(fields)):
 						clip_fitsfile(model='%s-initmodel.fits'%(fields[i+1][m]), 
 						          im='%s-initmodel.fits'%(fields[i+1][m]),
 						          snr=10.0)
-						os.system('%s -name %s-initmodel -reorder -predict -weight natural -field %s %s'%(";".join(params['global']["wsclean_command"]),fields[i+1][m],msinfo['FIELD']['fieldtoID'][fields[i+1][m]],msfile))
+						os.system('%s -name %s-initmodel -reorder -predict -weight natural %s -field %s %s'%(";".join(params['global']["wsclean_command"]),fields[i+1][m],mtmfs_wsclean, msinfo['FIELD']['fieldtoID'][fields[i+1][m]],msfile))
 			if params['phase_referencing']['imager'] == 'tclean':
 				delims = []
 				for z in ['.psf','.image','.sumwt','.mask','.residual','.pb','.model']:
